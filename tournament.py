51d2a9de071c15f9fcbd49b2f59eec5b1f8511d4
Guillaume de Cumont <guillaumedecumont@gmail.com>
2019-02-04 19:23:00 +0100

    TME3.1

--- tournament.py
+++ tournament.py
@@ -21,7 +21,7 @@ def get_team(nb_players):
 
 if __name__ == '__main__':
     from soccersimulator import Simulation, show_simu
-    team1 = get_team(2)
+    team1 = get_team(1)
     team2 = get_team(2)
     
     simu = Simulation(team1,team2)
