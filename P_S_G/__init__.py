00b5d4a099b98b4171588d28f7d107b0ba0d7437
root <root@localhost.localdomain>
2019-02-24 13:12:52 +0100

    MAJ name

--- P_S_G/__init__.py
+++ P_S_G/__init__.py
@@ -21,7 +21,7 @@ from soccersimulator import SoccerTeam
 def get_team(nb_players):
     team = SoccerTeam(name="pSG")
     if nb_players == 1:
-        team.add("Mbappélaterreur", P_S_G.Defenseur())
+        team.add("Mbappé", P_S_G.Defenseur())
     if nb_players == 2:
         team.add("Pavardef", P_S_G.Strategy_Defense())
         team.add("def", P_S_G.Strategy_Defense())
@@ -30,7 +30,7 @@ def get_team(nb_players):
 def get_team1(nb_players):
     team = SoccerTeam(name="pSG")
     if nb_players == 1:
-        team.add("Mbappémagueule", P_S_G.Defenseur())
+        team.add("Mbappé2", P_S_G.Defenseur())
     if nb_players == 2:
         team.add("Pavardoff", P_S_G.Strategy_Attaque())
         team.add("Mbappé", P_S_G.Strategy_Attaque())
