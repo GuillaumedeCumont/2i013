4c77cffc3f54a641ba77458cbbbce298b1a5f27f
root <root@localhost.localdomain>
2019-03-23 15:48:03 +0100

    MAJMars4joueurs

--- tournament.py
+++ tournament.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 """
-Éditeur de Spyder
+Created on Thu Feb  7 16:14:40 2019
 
-Ceci est un script temporaire.
+@author: root
 """
 
 import P_S_G
 from soccersimulator import Simulation, show_simu
 from P_S_G import get_team, get_team1
 
-team1 = get_team1(4)
+team1 = get_team(4)
 team2 = get_team(4)
 
 simu = Simulation(team1, team2)
